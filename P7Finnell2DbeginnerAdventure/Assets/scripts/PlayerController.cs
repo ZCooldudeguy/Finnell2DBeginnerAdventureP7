bc6dd9b6078558de5d69e7285750ffe20da17a11
ZCooldudeguy <731256@student.dusd.net>
2025-11-05 15:29:53 -0800

    a
    a

--- P7Finnell2DbeginnerAdventure/Assets/scripts/PlayerController.cs
+++ P7Finnell2DbeginnerAdventure/Assets/scripts/PlayerController.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeftAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        float horizontal = 0.0f;
+        if (LeftAction.IsPressed())
+            {
+            horizontal = -1.0f;
+            }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
+            Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
+        
     }
 }
