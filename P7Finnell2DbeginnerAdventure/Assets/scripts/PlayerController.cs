0b882c42e2f9ea5aae9564a75791b873c1fcb1fd
ZCooldudeguy <731256@student.dusd.net>
2025-11-20 15:27:35 -0800

    health,colliders
    added health max and colliders to decor

--- P7Finnell2DbeginnerAdventure/Assets/scripts/PlayerController.cs
+++ P7Finnell2DbeginnerAdventure/Assets/scripts/PlayerController.cs
@@ -9,26 +9,28 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
-
+    public int maxHealth = 5;
+    int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+        
     }
 
 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.9f * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
 }
